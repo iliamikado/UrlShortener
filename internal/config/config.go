975ca337383dde526c1b57fec3fd83e0507e9661
iliamikado <iliamikado@gmail.com>
2024-02-07 23:04:56 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var LaunchAddress string
@@ -11,4 +12,11 @@ func ParseConfig() {
 	flag.StringVar(&LaunchAddress, "a", "localhost:8080", "Set launch address for server")
 	flag.StringVar(&ResultAddress, "b", "http://localhost:8080", "Set basic result address for short URL")
 	flag.Parse()
+
+	if serverAddress := os.Getenv("SERVER_ADDRESS"); serverAddress != "" {
+		LaunchAddress = serverAddress
+	}
+	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+		ResultAddress = baseURL
+	}
 }
